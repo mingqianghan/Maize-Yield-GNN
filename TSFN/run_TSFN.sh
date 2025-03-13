4ad9b369cffd777f1651adff4602f5617dc916de
Mingqiang Han <49960231+mingqianghan@users.noreply.github.com>
2025-03-12 20:46:06 -0500

    change run parameters

--- TSFN/run_TSFN.sh
+++ TSFN/run_TSFN.sh
@@ -5,4 +5,4 @@
 # google colab
 DATA_PATH="/content/drive/MyDrive/Colab Notebooks/Maize Yield Prediction/dataset/image_dataset.pkl"
 
-python main.py --lr 0.01 --data_path "$DATA_PATH"
+python main.py --lr 0.1 --data_path "$DATA_PATH"
