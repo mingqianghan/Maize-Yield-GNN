c6da535cf53dcfbc4578c13e1cf6dcea330ee98a
Mingqiang Han <49960231+mingqianghan@users.noreply.github.com>
2025-03-14 17:35:41 -0500

    change run parameter

--- TSFN/run_TSFN.sh
+++ TSFN/run_TSFN.sh
@@ -8,4 +8,4 @@ DATA_PATH="/content/drive/MyDrive/Colab Notebooks/Maize Yield Prediction/dataset
 #python main.py --lr 0.001 --timepoints R1 R2 --data_path "$DATA_PATH"`
 #python main.py --lr 0.001 --timepoints R1 R2 R3 R4 --data_path "$DATA_PATH"
 #python main.py --lr 0.001 --timepoints R1 R2 R3 --data_path "$DATA_PATH"
-python main.py --lr 0.0001 --timepoints R1 R2 R3 R4 R5 R6 --data_path "$DATA_PATH"
+python main.py --lr 0.0005 --timepoints R1 R2 R3 R4 R5 R6 --data_path "$DATA_PATH"
