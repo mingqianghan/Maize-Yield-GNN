1e54db226063aad1f2e949fcefa2a9d556e596d0
Mingqiang Han <49960231+mingqianghan@users.noreply.github.com>
2025-03-13 19:22:09 -0500

    Fix errors

--- TSFN/run_TSFN.sh
+++ TSFN/run_TSFN.sh
@@ -5,5 +5,5 @@
 # google colab
 DATA_PATH="/content/drive/MyDrive/Colab Notebooks/Maize Yield Prediction/dataset/image_dataset.pkl"
 
-python main.py --lr 0.001 --timepoints ["R1", "R2"] --data_path "$DATA_PATH"
-python main.py --lr 0.001 --timepoints ["R1", "R2", "R3"] --data_path "$DATA_PATH"
+python main.py --lr 0.001 --timepoints R1 R2 --data_path "$DATA_PATH"
+python main.py --lr 0.001 --timepoints R1 R2 R3 --data_path "$DATA_PATH"
