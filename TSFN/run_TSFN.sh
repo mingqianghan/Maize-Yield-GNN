a8e75ceac57c80cfa79c6b800528d398cbbadda5
Mingqiang Han <49960231+mingqianghan@users.noreply.github.com>
2025-03-15 12:10:32 -0500

    change run parameters

--- TSFN/run_TSFN.sh
+++ TSFN/run_TSFN.sh
@@ -5,8 +5,8 @@
 # google colab
 DATA_PATH="/content/drive/MyDrive/Colab Notebooks/Maize Yield Prediction/dataset/image_dataset.pkl"
 
-#python main.py --lr 0.001 --lr_patience 25 --timepoints R1 R2 R3 R4 R5 R6 --data_path "$DATA_PATH"
-python main.py --lr 0.01 --lr_patience 25 --timepoints R2 R3 R4 R5 R6  --data_path "$DATA_PATH"
+python main.py --lr 0.01 --lr_patience 25 --timepoints R1 R2 R3 R4 R5 R6 --data_path "$DATA_PATH"
+#python main.py --lr 0.01 --lr_patience 25 --timepoints R2 R3 R4 R5 R6  --data_path "$DATA_PATH"
 #python main.py --lr 0.001 --lr_patience 25 --timepoints R1 R3 R4 R5 R6 --data_path "$DATA_PATH"
 #python main.py --lr 0.001 --lr_patience 25 --timepoints R1 R2 R4 R5 R6 --data_path "$DATA_PATH"
 #python main.py --lr 0.001 --lr_patience 25 --timepoints R1 R2 R3 R5 R6 --data_path "$DATA_PATH"
